12da51d19c6aa24cc50752ce1c8e00b607717ba2
louis09392003 <louis09392003@gmail.com>
2021-10-12 12:07:46 -0600

    envior setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`Got ${req.method} on ${req.url}`);
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Listening on http://localhost:' + port + '/');
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client', 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$$/,
+        include: [
+          path.join(__dirname, 'client', 'src')
+        ],
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
